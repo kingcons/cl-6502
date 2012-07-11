08eacd83ae66d43a7e7e3a79c26675d9b5e89c11
Brit Butler <redline6561@gmail.com>
2012-07-11 18:50:58 -0400

    Improve *benchmark* and its documentation.

--- src/toys.lisp
+++ src/toys.lisp
@@ -8,16 +8,10 @@
     #xa9 #x00  ; $0002 a9 00 LDA #$00
     #xa2 #x00  ; $0004 a2 00 LDX #$00
     #xa0 #x00  ; $0006 a0 00 LDY #$00
-    ;; Increment Y, then branch to 0x08
+    ;; Increment Y until it wraps around to zero, then proceed to 0x0b
     #xc8       ; $0008 c8    INY
     #xd0 #xfd  ; $0009 d0 fd BNE $0008
-    ;; Increment X until zero bit set, branch to 0x06, delete y and loop
-    #xe8       ; $000b e8    INX
-    #xd0 #xf8  ; $000c d0 f8 BNE $0006
-    ;; Add 1 with carry to accumulator and rewind to 0x04
-    #x69 #x01  ; $000e 69 01 ADC #$01
-    #xd0 #xf2  ; $0010 d0 f2 BNE $0004
-    ;; set break and interrupt, set PC to 0, subtract from accumulator.
-    #x00       ; $0012 00    BRK
-    #xed)      ; $0013 ed    SBC
-  "Should leave the Y register with the value 47.")
+    ;; subtract the sum of value at 0x01+carry from accumulator and halt.
+    #xed #x01  ; $000b ed 01 SBC #$01
+    #x00)      ; $000d 00    BRK
+  "Should leave the Accumulator with the value 231.")
