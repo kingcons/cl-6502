89670164d45e44b9c859a1c9f2670bb347663461
Brit Butler <redline6561@gmail.com>
2012-07-13 21:27:28 -0400

    Fix Label and Var handling.

--- src/assemble.lisp
+++ src/assemble.lisp
@@ -17,26 +17,32 @@
   (let ((trimmed-src (trim-whitespace src)))
     (trim-whitespace (subseq trimmed-src 0 (position #\; trimmed-src)))))
 
+(defun extract-num (str mode)
+  "Extract a hex number from its containing string, STR."
+  (let ((token (cl-ppcre:scan-to-strings "[0-9a-fA-F]{2,4}" str)))
+    (cond ((and token (member mode '(absolute absolute-x absolute-y indirect)))
+           (mapcar (lambda (x) (parse-integer x :radix 16))
+                   (list (subseq token 2) (subseq token 0 2))))
+          (token (parse-integer token :radix 16)))))
+
 (defun asm (source)
   "Assemble SOURCE string into a bytevector and return it."
   (apply 'concatenate 'vector
-         (loop for line in (cl-ppcre:split "\\n" source)
-            collecting (asm-instruction (extract-code line)))))
+         (loop for line in (mapcar #'extract-code (cl-ppcre:split "\\n" source))
+            for pc = 0 then (+ pc (length (cl-ppcre:split "\\ " line)))
+            collecting (asm-instruction line pc))))
 
-(defun asm-instruction (code)
+(defun asm-instruction (code pc)
   "Assemble CODE into a bytevector, using the *CONTEXT* as necessary."
   (let ((label-p (position #\: code))
         (store-p (position #\= code))
         (tokens (cl-ppcre:split "\\ " code)))
     (flet ((set-var (delimiter &optional default)
-             (destructuring-bind (var val &rest src)
-                 (cl-ppcre:split delimiter code)
-               (setf (gethash var *symbol-table*) (or default val))
-               (setf tokens (cl-ppcre:split "\\ " src)))))
-      ; TODO: Track 'Program Counter' for Labels. Will need to be done in ASM.
-      (cond (label-p (set-var "\\:" 0))
-            (store-p (set-var "\\=")))
-      (process-tokens tokens))))
+             (destructuring-bind (var &rest src) (cl-ppcre:split delimiter code)
+               (setf (gethash var *symbol-table*) (or default (first src)))) nil))
+      (cond (label-p (set-var "\\:" (format nil "$~4,'0x" pc)))
+            (store-p (set-var "\\="))
+            (t (process-tokens tokens))))))
 
 (defun find-opcode (name mode)
   "Find an opcode matching NAME and MODE, raising ILLEGAL-OPCODE otherwise."
@@ -50,44 +56,45 @@
         (error 'illegal-opcode :opcode (format nil "Name:~A Mode:~A" name mode)))
       (first result))))
 
+(defun match-mode (str)
+  "Given a string, STR, match it to an addressing mode returning that or NIL."
+  (let ((regex-modes '(("^$" . implied)
+                       ("^[aA]$" . accumulator)
+                       ("^#\\$[0-9a-fA-F]{2}$" . immediate)
+                       ("^\\$[0-9a-fA-F]{4},[xX]$" . absolute-x)
+                       ("^\\$[0-9a-fA-F]{4},[yY]$" . absolute-y)
+                       ("^\\$[0-9a-fA-F]{4}$" . absolute)
+                       ("^\\$[0-9a-fA-F]{2},[xX]$" . zero-page-x)
+                       ("^\\$[0-9a-fA-F]{2},[yY]$" . zero-page-y)
+                       ("^\\$[0-9a-fA-F]{2}$" . zero-page)
+                       ("^\\(\\$[0-9a-fA-F]{2}\\),[xX]$" . indirect-x)
+                       ("^\\(\\$[0-9a-fA-F]{2}\\),[yY]$" . indirect-y)
+                       ("^\\(\\$[0-9a-fA-F]{4}\\)$" . indirect)
+                       ("^&[0-9a-fA-F]{2,4}$" . relative))))
+    (loop for (regex . mode) in regex-modes
+       when (cl-ppcre:scan regex str)
+       return mode)))
+
 (defun find-mode (tokens)
   "Determine which mode is being used by TOKENS, raising UNKNOWN-MODE otherwise."
   (let* ((token-str (apply 'concatenate 'string tokens))
-         (regex-modes '(("^$" . implied)
-                        ("^[aA]$" . accumulator)
-                        ("^#\\$[0-9a-fA-F]{2}$" . immediate)
-                        ("^\\$[0-9a-fA-F]{4},[xX]$" . absolute-x)
-                        ("^\\$[0-9a-fA-F]{4},[yY]$" . absolute-y)
-                        ("^\\$[0-9a-fA-F]{4}$" . absolute)
-                        ("^\\$[0-9a-fA-F]{2},[xX]$" . zero-page-x)
-                        ("^\\$[0-9a-fA-F]{2},[yY]$" . zero-page-y)
-                        ("^\\$[0-9a-fA-F]{2}$" . zero-page)
-                        ("^\\(\\$[0-9a-fA-F]{2}\\),[xX]$" . indirect-x)
-                        ("^\\(\\$[0-9a-fA-F]{2}\\),[yY]$" . indirect-y)
-                        ("^\\(\\$[0-9a-fA-F]{4}\\)$" . indirect)
-                        ("^&[0-9a-fA-F]{2,4}$" . relative)))
-         (result (loop for (regex . mode) in regex-modes
-                    when (cl-ppcre:scan regex token-str)
-                    return mode)))
-    (unless result
-      (error 'unknown-mode :tokens tokens))
-    result))
+         (result (if (gethash token-str *symbol-table*)
+                     (match-mode (gethash token-str *symbol-table*))
+                     (match-mode token-str))))
+    (or result (error 'unknown-mode :tokens tokens))))
 
 (defun process-tokens (tokens)
   "Takes a tokenized block of code and generates an appropriate 6502 bytevector."
   (let* ((mnemonic (first tokens))
          (mode (find-mode (rest tokens)))
          (args (loop for token in (rest tokens)
-                  for arg = (cl-ppcre:scan-to-strings "[0-9a-fA-F]{2,4}" token)
-                  if (member mode '(absolute absolute-x absolute-y indirect))
-                    return (mapcar (lambda (x) (parse-integer x :radix 16))
-                                   (list (subseq arg 2) (subseq arg 0 2)))
-                  if arg
-                    collect (parse-integer arg :radix 16)
+                  for sym = (gethash token *symbol-table*)
+                  for arg = (extract-num token mode)
+                  if sym collect (extract-num sym mode)
+                  else if arg collect arg
                   else unless (cl-ppcre:scan "^[aAxXyY]" token)
-                    collect (or (gethash token *symbol-table*)
-                                (error 'syntax-error :token token
-                                       :line (format nil "~{~A ~}" tokens))))))
-    (if mnemonic
-        (concatenate 'vector (vector (find-opcode mnemonic mode)) args)
+                  return (error 'syntax-error :token token
+                                :line (format nil "~{~A ~}" tokens)))))
+    (if (and mnemonic (not (gethash mnemonic *symbol-table*)))
+        (apply 'vector (find-opcode mnemonic mode) (flatten args))
         #())))
