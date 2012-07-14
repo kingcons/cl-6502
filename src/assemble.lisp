9608b9b46c49225940d9441aa9291352b80eeb09
Brit Butler <redline6561@gmail.com>
2012-07-13 21:36:49 -0400

    Minor cleanups and fixes. Make this code suck less tomorrow.

--- src/assemble.lisp
+++ src/assemble.lisp
@@ -20,20 +20,26 @@
 (defun extract-num (str mode)
   "Extract a hex number from its containing string, STR."
   (let ((token (cl-ppcre:scan-to-strings "[0-9a-fA-F]{2,4}" str)))
-    (cond ((and token (member mode '(absolute absolute-x absolute-y indirect)))
-           (mapcar (lambda (x) (parse-integer x :radix 16))
-                   (list (subseq token 2) (subseq token 0 2))))
-          (token (parse-integer token :radix 16)))))
+    (flet ((parse-hex (x) (parse-integer x :radix 16)))
+      (when token
+        (if (member mode '(absolute absolute-x absolute-y indirect))
+            (mapcar #'parse-hex (list (subseq token 2) (subseq token 0 2)))
+            (parse-hex token))))))
 
 (defun asm (source)
   "Assemble SOURCE string into a bytevector and return it."
-  (apply 'concatenate 'vector
-         (loop for line in (mapcar #'extract-code (cl-ppcre:split "\\n" source))
-            for pc = 0 then (+ pc (length (cl-ppcre:split "\\ " line)))
-            collecting (asm-instruction line pc))))
+  (flet ((bytes (line)
+           (if (or (position #\: line) (position #\= line))
+               0
+               (length (cl-ppcre:split "\\ " line)))))
+    (apply 'concatenate 'vector
+           (loop for line in (mapcar #'extract-code (cl-ppcre:split "\\n" source))
+              for pc = 0 then (+ pc (bytes line))
+              collecting (asm-instruction line pc)))))
 
 (defun asm-instruction (code pc)
   "Assemble CODE into a bytevector, using the *CONTEXT* as necessary."
+  (print pc)
   (let ((label-p (position #\: code))
         (store-p (position #\= code))
         (tokens (cl-ppcre:split "\\ " code)))
