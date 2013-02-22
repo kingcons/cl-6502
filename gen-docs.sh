e84a9ad903879551fec12bb3e776ce0c1ecefcdb
Brit Butler <redline6561@gmail.com>
2013-02-22 12:44:15 -0500

    Docs update.

--- gen-docs.sh
+++ gen-docs.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 sbcl --eval "(ql:quickload '(cl-6502 sb-introspect cl-api))" \
      --eval "(cl-api:api-gen :cl-6502 \"docs/cl-6502.html\")" \
-     --eval "(cl-api:api-gen :6502-cpu \"docs/6502-cpu.html\")" \
      --eval "(progn (terpri) (sb-ext:quit))"
