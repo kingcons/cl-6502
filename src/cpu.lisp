ec880a5c603c96d3fa40321e7592626aaf29f0d9
Brit Butler <redline6561@gmail.com>
2013-02-22 22:28:18 -0500

    Nope. This API is better.

--- src/cpu.lisp
+++ src/cpu.lisp
@@ -35,14 +35,16 @@
 ;;; Helpers
 
 (defmacro defenum (name (&rest keys))
-  "Define a function named %NAME, that takes an argument KEY. If KEY is a fixnum,
-return the value at KEY. Otherwise, return the index of KEY. KEYS may consist of
-non-numeric scalar values or lists that start with an EQL-testable scalar value."
-  `(defun ,(intern (format nil "%~A" (string-upcase name))) (key)
-     (let ((enum ',keys))
-       (if (integerp key)
-           (nth key enum)
-           (position key enum ,@(when (typep (car keys) 'list) '(:key #'first)))))))
+  "Define a function named %NAME, that takes args (KEY &OPTIONAL NEXT). If NEXT is
+non-nil, return the successor to KEY. Othewrise, return the index of KEY. KEYS may
+consist of scalar values or lists that start with a scalar value."
+  `(defun ,(intern (format nil "%~A" (string-upcase name))) (key &optional next)
+     (let* ((enum ',keys)
+            (val (position key enum ,@(when (typep (car keys) 'list)
+                                            '(:key #'first)))))
+       (if next
+           (nth (1+ val) enum)
+           val))))
 
 (defgeneric reset (obj)
   (:documentation "Reset the OBJ to an initial state.")
