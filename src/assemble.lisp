7a973727f80f4ed5f7b47f4b83cf3cbd6abd0d6a
Brit Butler <redline6561@gmail.com>
2013-03-17 02:00:21 -0400

    Drop label and comment support.

--- src/assemble.lisp
+++ src/assemble.lisp
@@ -22,27 +22,6 @@
   (let ((code (subseq line 0 (position #\; line))))
     (cl-ppcre:split "\\ " (string-trim '(#\Tab #\Return #\Space) code))))
 
-(defun preprocess-p (tokens)
-  "Take a list of TOKENS and return T if it contains a preprocessor statement."
-  (some (lambda (x) (search x (first tokens))) '(":" "=")))
-
-(defun preprocess (statement pc)
-  "Preprocess the STATEMENT setting values in the *SYMTABLE* accordingly."
-  (flet ((set-var (delimiter &optional val)
-           (destructuring-bind (var &rest src) (cl-ppcre:split delimiter statement)
-             (setf (gethash var *symtable*) (or val (first src))))))
-    (cond ((search ":" statement) (set-var "\\:" pc))
-          ((search "=" statement) (set-var "\\="))
-          (t (error 'invalid-syntax :line statement)))))
-
-(defun next-pc (line pc)
-  "Compute the new program counter given LINE and PC."
-  (let ((mode (match-mode (rest line))))
-    (cond ((or (preprocess-p line) (emptyp line)) pc)
-          (mode (+ pc (third (find-opcode (first line) mode t))))
-          ((some (lambda (x) (search x (second line))) '("&" "#$")) (+ pc 2))
-          (t (+ pc 3)))))
-
 (defgeneric asm (source)
   (:documentation "Assemble SOURCE into a bytevector and return it.")
   (:method :before (source) (clrhash *symtable*)))
@@ -58,16 +37,14 @@
   (apply 'concatenate 'vector (mapcar #'process-statement source)))
 
 (defmacro with-src-pass ((src) &body body)
-  "Loop over SRC, tracking the PC and binding LINE. BODY should be a LOOP expr."
-  `(loop for pc = 0 then (next-pc line pc)
-      for line in (mapcar #'tokenize (cl-ppcre:split "\\n" ,src))
+  "Loop over SRC binding LINE. BODY should be a LOOP expr."
+  `(loop for line in (mapcar #'tokenize (cl-ppcre:split "\\n" ,src))
         ,@body))
 
 (defmethod asm ((source string))
-  (with-src-pass (source) if (preprocess-p line) do (preprocess (first line) pc))
   (let ((results (with-src-pass (source)
-                   unless (or (preprocess-p line) (emptyp line))
-                   collect (process-tokens line pc))))
+                   unless (emptyp line)
+                   collect (process-tokens line))))
     (apply 'concatenate 'vector results)))
 
 (defun find-opcode (name mode &optional return-match-p)
@@ -95,39 +72,13 @@ SEXP-SYNTAX-P should be t when tokens are in assembler's SEXP-based syntax."
     (loop for mode in (remove-duplicates (map 'list #'fourth *opcodes*))
        when (cl:and mode (cl-ppcre:scan (reader mode) line)) return mode)))
 
-(defun prefix-of (str)
-  "Extract the mode prefix of STR."
-  (subseq str 0 (1+ (loop for char in '(#\# #\$ #\( #\&)
-                     maximizing (or (position char str) 0)))))
-
-(defun compute-offset (label pc)
-  "Compute the offset for a relative branch from PC to LABEL."
-  (wrap-byte (if (> label pc)
-                 (- label pc)
-                 (- #xff (- pc label)))))
-
-(defun splice-sym (val prefix pc)
-  "Splice together VAL and PREFIX, computing an offset if needed."
-  (let ((spliced (if (string= prefix "&")
-                     (format nil "~2,'0x" (compute-offset val pc))
-                     (format nil "~4,'0x" val))))
-    (format nil "~a~a" prefix spliced)))
-
-(defun resolve (tokens pc)
-  "Given TOKENS and PC, resolve any symbols in TOKENS."
-  (flet ((lookup (sym)
-           (gethash (string-trim '(#\# #\$ #\( #\&) sym) *symtable*)))
-    (loop for token in tokens for sym = (lookup token)
-       if sym collect (splice-sym sym (prefix-of token) (1+ pc))
-       else collect token)))
-
 (defun process-args (tokens mode)
   "Take a list of args TOKENS and produce an appropriate 6502 bytevector."
   (remove nil (flatten (mapcar (lambda (x) (extract-num x mode)) tokens))))
 
-(defun process-tokens (tokens pc)
+(defun process-tokens (tokens)
   "Takes a tokenized block of code and generates an appropriate 6502 bytevector."
-  (let* ((args (resolve (rest tokens) pc))
+  (let* ((args (rest tokens))
          (mode (match-mode args)))
     (if mode
         (apply 'vector (find-opcode (first tokens) mode) (process-args args mode))
