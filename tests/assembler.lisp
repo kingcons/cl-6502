e8ce81a5f262da65fb8623b9ed56d11c25ba1171
Brit Butler <redline6561@gmail.com>
2013-03-17 01:51:11 -0400

    Update tests for assembler overhaul.

--- tests/assembler.lisp
+++ tests/assembler.lisp
@@ -97,37 +97,20 @@
   (is (equalp (asm "  ; blah blah blah") #()))
   (is (equalp (asm "  BRK ; foo bar baz") #(0))))
 
-(deftest assemble-label
-    "Labels ($LABEL:) should store a reference to the current PC."
-  (let ((code (format nil " loop: ~% LDY #$00~% INY~% jmp $loop")))
-    (is (equalp (asm code) (bvec a0 00 c8 4c 00 00)))))
-
-(deftest assemble-data
-    "Variables ($VAR=value) should store values."
-  (let ((code (format nil " std_hours=12~% lda #$std_hours~%
-                            mil-hours=24~% ldy #$mil-hours")))
-    (is (equalp (asm code) #(#xa9 18 #xa0 36)))))
-
 (deftest assemble-program
     "A basic program should assemble correctly."
   (let ((code (format nil "CLC~% LDA #$00~% LDY #$00~%
-                           loop:~% INY~% bne &loop~% sbc $0001~% brk")))
+                           INY~% bne &fd~% sbc $0001~% brk")))
     (is (equalp (asm code) #(24 169 0 160 0 200 208 253 237 1 0 0)))
     (cl-6502:execute cpu (asm code))
     (is (eql (cpu-ar cpu) 86))))
 
 (deftest assemble-forward-relative
     "A program with a forward relative jump should assemble correctly."
-  (let ((code (format nil "CLC~% LDA #$00~% LDY #$00~% bne &loop~% nop~% nop~%
-                           loop:~% INY~% bne &loop~% sbc $0001~% brk")))
+  (let ((code (format nil "CLC~% LDA #$00~% LDY #$00~% bne &02~% nop~%
+                           nop~% INY~% bne &fd~% sbc $0001~% brk")))
     (cl-6502:execute cpu (asm code))
     (is (eql (cpu-ar cpu) 86))))
-
-(deftest assemble-forward-refs
-    "A program with forward references should assemble correctly."
-  (let ((code (format nil "jmp $loop~% CLC~% LDA #$00~% LDY #$00~%
-                           loop:~% INY~% bne &loop~% sbc $0001~% brk")))
-    (is (equalp (asm code) #(76 8 0 24 169 0 160 0 200 208 253 237 1 0 0)))))
 
 (deftest assemble-symbolic
     "A sexp-format program should assemble correctly."
